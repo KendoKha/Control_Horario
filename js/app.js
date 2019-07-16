fbac82c080c8ba659a26c7af29ce50c190258b86
rpf1980 <united_ssh@outlook.com>
2019-07-16 20:13:32 +0200

    chore(): add functions related to buttons

---
+++ js/app.js
@@ -0,0 +1,32 @@
+//const btnIniciar = document.getElementById('idBtnIniciar');
+
+// Función botón inicio
+function iniciarApp() {
+
+    window.location.href = "/html/entry.html";
+}
+
+// Función que nos cambia texto de elementos ( que contengas texto, evidentemente)
+function changeText(node, newText) {
+
+    return node.textContent = newText;
+}
+
+window.addEventListener('click', function(event) {
+    const img = document.querySelector('img');
+    const buttonValue = event.target.textContent;
+    const button = this.document.querySelector('button');
+    const h1 = this.document.querySelector('h1');
+
+    if (buttonValue === 'FICHAR') {
+        img.src = "../img/pause-round-button.png";
+
+        changeText(h1, "Working ...")
+            //return button.textContent = "PAUSE";
+        return changeText(button, "PAUSE");
+    }
+
+    img.src = "../img/play-button.png";
+    changeText(h1, "Pausing ...")
+    changeText(button, "PLAY");
+});
