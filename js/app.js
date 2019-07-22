1604950b40800f511c31aef442fb8b802d1e1b24
rpf1980 <united_ssh@outlook.com>
2019-07-22 09:07:42 +0200

    chore(): add comments on js file

--- js/app.js
+++ js/app.js
@@ -10,18 +10,19 @@ function changeText(node, newText) {
     return node.textContent = newText;
 }
 
+// Agregamos un controlador de eventos al objeto ventana
 window.addEventListener('click', function(event) {
-    const img = document.querySelector('img');
-    const buttonValue = event.target.textContent;
-    const button = this.document.querySelector('button');
-    const h1 = this.document.querySelector('h1');
+    const img = document.querySelector('img'); // Guarda referencia al nodo image usando querySelector
+    const buttonValue = event.target.textContent; // Guarda el valor del texto del nodo button
+    const button = this.document.querySelector('button'); // Guarda referencia al nodo button 
+    const h1 = this.document.querySelector('h1'); // Guarda referencia al nodo h1
 
-    if (buttonValue === 'FICHAR') {
-        img.src = "../img/pause-round-button.png";
+    if (buttonValue === 'FICHAR') { // Si el texto del nodo button conincide con la condición
+        img.src = "../img/pause-round-button.png"; // ... cambia imagen 
 
-        changeText(h1, "Working ...")
+        changeText(h1, "Working ...") // ... y cambia texto del h1
             //return button.textContent = "PAUSE";
-        return changeText(button, "PAUSE");
+        return changeText(button, "PAUSE"); // ... además cambia texto del botón
     }
 
     img.src = "../img/play-button.png";
